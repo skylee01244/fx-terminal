2534a065eca79c75f6ba39a6a82a11eddf49817a
skylee01244 <skylee01244@gmail.com>
2026-01-02 03:26:19 +1100

    export wrapper for screens

---
+++ bot/terminal_screens/__init__.py
@@ -0,0 +1,6 @@
+from .trading_screen import TradingScreen
+from .analysis_screen import AnalysisScreen
+from .portfolio_screen import PortfolioScreen
+from .orders_screen import OrdersScreen
+
+__all__ = ['TradingScreen', 'AnalysisScreen', 'PortfolioScreen', 'OrdersScreen']
