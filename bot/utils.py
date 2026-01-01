c30bc9597fea934ce80e7ed3150ac973b66e4700
skylee01244 <skylee01244@gmail.com>
2026-01-02 02:14:04 +1100

    Deleted test files and dateandtime parser

--- bot/utils.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-def format_datetime(dt_str):
-    """Convert Saxo's ISO datetime into a cleaner format"""
-    return datetime.fromisoformat(dt_str.replace("Z", "+00:00")).strftime("%Y-%m-%d %H:%M UTC")
